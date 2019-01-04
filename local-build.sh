39398afedce90c1d28b6e3cefa5e5b5914515568
Hamilton Lima <hamilton.lima@gmail.com>
2019-01-04 07:38:34 -0500

    Adds database variables to local dev script

--- local-build.sh
+++ local-build.sh
@@ -3,5 +3,10 @@ export REDIS_HOST=localhost
 export REDIS_PORT=6379
 export INTERNAL_API_KEY=9239
 
+export MYSQL_ROOT_PASSWORD=foo123123
+export MYSQL_DATABASE=robolucha_db
+export MYSQL_USER=robolucha_uzr
+export MYSQL_PASSWORD=foo123123
+
 go build -o $HOME/go/bin/robolucha-api
 $HOME/go/bin/robolucha-api
