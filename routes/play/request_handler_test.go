bc99d182964d4cb2ac0540466ade2dce26cbfed5
Hamilton Lima <hamilton.lima@gmail.com>
2019-08-18 15:16:35 -0400

    Fixes unit test to support test implementation

--- routes/play/request_handler_test.go
+++ routes/play/request_handler_test.go
@@ -1,6 +1,7 @@
 package play_test
 
 import (
+	"gitlab.com/robolucha/robolucha-api/model"
 	"gitlab.com/robolucha/robolucha-api/routes/play"
 	"gotest.tools/assert"
 	"testing"
@@ -9,17 +10,17 @@ import (
 func TestPlayRequestHandler(t *testing.T) {
 	handler := play.Listen()
 
-	s1 := handler.Send(play.Request{Data: "foo"})
-	s2 := handler.Send(play.Request{Data: "bar"})
+	s1 := handler.Send(play.Request{Data: &model.AvailableMatch{ID: 42}})
+	s2 := handler.Send(play.Request{Data: &model.AvailableMatch{ID: 43}})
 
 	r1 := <-s1
 	r2 := <-s2
 
-	assert.Equal(t, "foo", r1.Data)
-	assert.Equal(t, "bar", r2.Data)
+	assert.Equal(t, uint(42), r1.Data.MatchID)
+	assert.Equal(t, uint(43), r2.Data.MatchID)
 
-	s3 := handler.Send(play.Request{Data: "other"})
+	s3 := handler.Send(play.Request{Data: &model.AvailableMatch{ID: 3}})
 	r3 := <-s3
-	assert.Equal(t, "other", r3.Data)
+	assert.Equal(t, uint(3), r3.Data.MatchID)
 
 }
