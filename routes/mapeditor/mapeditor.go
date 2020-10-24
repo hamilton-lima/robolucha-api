936ef8ab9a69a2d2558c6a3462bc3470c01be545
Hamilton Lima <hamilton.lima@gmail.com>
2020-10-24 17:15:50 -0400

    Adds POST and PUT

--- routes/mapeditor/mapeditor.go
+++ routes/mapeditor/mapeditor.go
@@ -1,11 +1,15 @@
 package mapeditor
 
 import (
+	"errors"
 	"net/http"
+
+	log "github.com/sirupsen/logrus"
 
 	"github.com/gin-gonic/gin"
 	"gitlab.com/robolucha/robolucha-api/datasource"
 	"gitlab.com/robolucha/robolucha-api/httphelper"
+	"gitlab.com/robolucha/robolucha-api/model"
 	"gitlab.com/robolucha/robolucha-api/pubsub"
 )
 
@@ -45,9 +49,8 @@ type Router struct {
 // Setup definition
 func (router *Router) Setup(group *gin.RouterGroup) {
 	group.GET("/mapeditor", getMyGameDefinitions)
-	// group.POST("/mapeditor", addMyGameDefinition)
-	// group.PATCH("/mapeditor/:id", updateMyGameDefinition)
-	// group.DELETE("/mapeditor/:id", delMyGameDefinition)
+	group.POST("/mapeditor", addMyGameDefinition)
+	group.PUT("/mapeditor", updateMyGameDefinition)
 }
 
 // getMyGameDefinitions godoc
@@ -61,4 +64,114 @@ func getMyGameDefinitions(c *gin.Context) {
 	user := httphelper.UserDetailsFromContext(c)
 	gameDefinitions := requestHandler.ds.FindGameDefinitionByOwner(user.User.ID)
 	c.JSON(http.StatusOK, gameDefinitions)
+}
+
+// addMyGameDefinition godoc
+// @Summary add a single gamedefition for this user
+// @Accept json
+// @Produce json
+// @Param request body model.GameDefinition true "GameDefinition"
+// @Success 200 {string} string
+// @Security ApiKeyAuth
+// @Router /private/mapeditor [post]
+func addMyGameDefinition(c *gin.Context) {
+	user := httphelper.UserDetailsFromContext(c)
+
+	// parse body parameter
+	var gameDefinition *model.GameDefinition
+	err := c.BindJSON(&gameDefinition)
+	if err != nil {
+		log.Info("Invalid body content on addMyGameDefinition")
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	err = requestHandler.Add(user.User.ID, gameDefinition)
+	if err != nil {
+		c.AbortWithStatus(http.StatusConflict)
+	} else {
+		c.JSON(http.StatusOK, "")
+	}
+
+}
+
+// updateMyGameDefinition godoc
+// @Summary update gamedefition for this user
+// @Accept json
+// @Produce json
+// @Param request body model.GameDefinition true "GameDefinition"
+// @Success 200 {array} model.GameDefinition
+// @Security ApiKeyAuth
+// @Router /private/mapeditor [put]
+func updateMyGameDefinition(c *gin.Context) {
+	user := httphelper.UserDetailsFromContext(c)
+
+	// parse body parameter
+	var gameDefinition *model.GameDefinition
+	err := c.BindJSON(&gameDefinition)
+	if err != nil {
+		log.Info("Invalid body content on addMyGameDefinition")
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	err = requestHandler.Update(user.User.ID, gameDefinition)
+	if err != nil {
+		c.AbortWithStatus(http.StatusConflict)
+	} else {
+		c.JSON(http.StatusOK, "")
+	}
+}
+
+func (handler *RequestHandler) Add(userID uint, gameDefinition *model.GameDefinition) error {
+	foundByName := handler.ds.FindGameDefinitionByName(gameDefinition.Name)
+	if foundByName != nil {
+		log.WithFields(log.Fields{
+			"gameDefinition": foundByName,
+		}).Info("gamedefinition already EXISTS with this name")
+		return errors.New("Gamedefinition name already exists")
+	} else {
+		gameDefinition.ID = 0
+		gameDefinition.OwnerUserID = userID
+
+		createResult := requestHandler.ds.CreateGameDefinition(gameDefinition)
+		log.WithFields(log.Fields{
+			"gameDefinition": createResult,
+		}).Info("gamedefinition CREATED")
+		return nil
+	}
+}
+
+func (handler *RequestHandler) Update(userID uint, gameDefinition *model.GameDefinition) error {
+	foundByID := handler.ds.FindGameDefinition(gameDefinition.ID)
+	// must exist to be updated
+	if foundByID == nil {
+		log.WithFields(log.Fields{}).Info("gamedefinition DOES NOT EXIST, cant be updated")
+		return errors.New("Gamedefinition DOES NOT exist")
+	}
+
+	// must be the owner to update it
+	if foundByID.ID != userID {
+		log.WithFields(log.Fields{}).Info("current user dont OWNS this gamedefinition, cant be updated")
+		return errors.New("current user DOES NOT OWN this Gamedefinition")
+	}
+
+	foundByName := handler.ds.FindGameDefinitionByName(gameDefinition.Name)
+	// name must not exist
+	if foundByName != nil && foundByName.ID != gameDefinition.ID {
+		log.WithFields(log.Fields{
+			"gameDefinition": foundByName,
+		}).Info("gamedefinition already EXISTS with this name")
+		return errors.New("gamedefinition already EXISTS with this name")
+
+	} else {
+		gameDefinition.OwnerUserID = userID
+
+		updateResult := handler.ds.UpdateGameDefinition(gameDefinition)
+		log.WithFields(log.Fields{
+			"gameDefinition": updateResult,
+		}).Info("gamedefinition UPDATED")
+		return nil
+	}
+
 }
